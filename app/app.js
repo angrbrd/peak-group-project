acc71f2686a2b8179064de9eab7e98da6187118f
Kristin Esterley <Kristin@Kristins-MBP.attlocal.net>
2017-04-29 08:57:14 -0400

    remove react-router

--- app/app.js
+++ app/app.js
@@ -1,11 +1,16 @@
-// Include the Main React Dependencies
+//Include the Main React Dependencies
 var React = require("react");
 var ReactDOM = require("react-dom");
 
 var Main = require("./components/Main");
 
-// Grabs the react routes
-var routes = require("./config/routes");
+// // Grabs the react routes
+// var routes = require("./config/routes");
 
 // Renders the contents according to the route page.
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<Main />, document.getElementById("app"));
+
+
+//change in branch kristin
+
+//new chage in branch kristin after stopped webpack
