5115f32b86e745616c5dcffe35da046eaa5e8af0
Kristin Esterley <Kristin@Kristins-MBP.attlocal.net>
2017-05-12 16:01:07 -0400

    no duplicates on objective selection

--- modules/AddStudentObjective.js
+++ modules/AddStudentObjective.js
@@ -9,6 +9,7 @@ export default React.createClass({
   getInitialState: function() {
     return { 
       goal_results: [],
+      student_results: [],
       objectives: [],
       student_id: this.props.params.studentId,
       student_name: this.props.params.studentName,
@@ -16,10 +17,99 @@ export default React.createClass({
     };
   },
 
+
+  componentDidMount: function(){
+    // get all of the schools from the schools table
+    console.log("inside componentDidMount");
+
+
+    helpers.getStudent(this.state.student_id).then(function(data) {
+      console.log("after helpers.getStudent");
+      console.log(data.data);
+        if (data.data !== this.state.student_results) {
+          this.setState({ student_results: data.data}); 
+          console.log("student_results in AddObjectives");
+          console.log(this.state.student_results);
+        }
+   
+
+
+        helpers.getObjectives(this.props.params.goalId).then(function(data) {
+
+          console.log(data.data);
+
+        // if (data.data !== this.state.results) {
+          this.setState({ goal_results: data.data });
+          this.setState({objectives: data.data[0].objectives});
+          this.setState({value: data.data[0].objectives[0]._id});                
+        
+        // This code is necessary to bind the keyword "this" when we say this.setState
+        // to actually mean the component itself and not the runQuery function.
+          console.log("after we got the objectives");
+        // console.log(this.state.student_results[0].goals[0].goal.description);
+        // console.log(this.state.goal_results[0].description);
+        // console.log(this.state.goal_results.length);
+
+        // console.log(this.state.student_results[0].goals.length);
+            
+          for (var i=0;i<this.state.student_results[0].goals.length;i++){
+
+            for(var j=0; j<this.state.student_results[0].goals[i].student_objectives.length;j++){
+
+                var tempObjectiveArr = this.state.objectives;
+
+                for (var k=0;k<tempObjectiveArr.length;k++){
+                  if (this.state.student_results[0].goals[i].student_objectives[j].objective.description == tempObjectiveArr[k].description){
+                    console.log(this.state.student_results[0].goals[i].student_objectives[j].objective.description);
+                    console.log(tempObjectiveArr[k].description);
+
+                      tempObjectiveArr.splice(k,1);
+                    break;           
+                  }
+
+                }// end k loop looking for a match in all of the objectives in the array of objectives for the given goal
+            this.setState({objectives: tempObjectiveArr});
+          } //end j loop for all of the objectives for the given goal already assigned to the student
+        } //end i loop for all of the goals assigned to the student
+  
+        // This code is necessary to bind the keyword "this" when we say this.setState
+        // to actually mean the component itself and not the runQuery function.
+      }.bind(this));  
+
+       }.bind(this));
+
+        // console.log("after we got the goals");
+        // console.log(this.state.student_results[0].goals[0].goal.description);
+        // console.log(this.state.goal_results[0].description);
+        // console.log(this.state.goal_results.length);
+
+        // console.log(this.state.student_results[0].goals.length);
+
+  },
+
+
+
+
+
+
+
+
+
+
+
   componentWillMount: function(){
     // get all of the schools from the schools table
     console.log("inside addStudentObjectives componentWillMount");
     console.log(this.props.params.goalId);
+
+
+    //need to get the objectives for this goal that the student has already been assigned.
+    //once those have been retreived, we get all of the objectives for the goal and put them in
+    // an array. 
+    //then remove from the array the objectives that the student has already been assigned.
+
+
+
 
     helpers.getObjectives(this.props.params.goalId).then(function(data) {
 
