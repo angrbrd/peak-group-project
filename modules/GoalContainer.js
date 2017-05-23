5488a68658c63582e7c7de252133d2e419244ac7
ChloeCacola <cscorrigan@icloud.com>
2017-05-23 18:13:01 -0400

    hide manage goal/manage

--- modules/GoalContainer.js
+++ modules/GoalContainer.js
@@ -49,9 +49,9 @@ export default React.createClass({
 		  <div>
         <div className="row" id="userNav">
 
-          <div className="col-sm-2">
+         {/*} <div className="col-sm-2">
             <Link to="/goals"><button id="manageGoals">Manage</button></Link>
-          </div>
+          </div>*/}
 
           <div className="col-sm-4 col-sm-offset-6">
           <p id="usernameDisplay">Kathleen McPeak</p>
